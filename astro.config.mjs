38938803d42a3a5c50181da8fe268a8c44998e2e
tsaijialing <tsaijialing1114@gmail.com>
2026-05-17 05:30:31 +0800

    Fix trailing slash asset path issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,6 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://tsaijialing.github.io",
+	build: { format: 'file' },
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
