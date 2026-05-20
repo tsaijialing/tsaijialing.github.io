3aa52562bad7d8eb9a271dadf3a7a023686743ad
tsaijialing <tsaijialing1114@gmail.com>
2026-05-21 02:06:59 +0800

    Fix article 404 by explicitly defining slug path

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "+0",
+	title: "+0 BLOG",
 	subtitle: "MY BLOG",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
